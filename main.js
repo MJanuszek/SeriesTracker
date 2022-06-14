211dc91168fac1be5348772525b4007f43d3670d
MJanuszek <monika.januszek.mail@gmail.com>
2022-06-14 13:56:06 +0200

    Search input is working
    Search input is working, adding div with found title

--- main.js
+++ main.js
@@ -2,14 +2,52 @@
 // div to add titles:
 const titleDisplay = document.querySelector(".title-display");
 const searchInput = document.querySelector(".search-input");
-const searchButton = document.querySelector(".search-button");
+// const searchButton = document.querySelector(".search-button");
+
+const searchButton = document.getElementById("search-button");
+// const searchInput = document.getElementById("search-input");
+
+const favTitles = ["gotham", "Nana", "code geass", "supernatural"]
+
+// const favTitles = [
+//     {
+//         title: "gotham",
+//     },
+
+//     {
+//         title: "code Geass",
+//     },
+
+//     {
+//         title: "supernaturals",
+//     },
 
 
 
+// ]
 
-const searchTitles = () => {
-    console.log("ok")
+const searchTitles = (e) => {
+    // przekazanie array do funkcj na eventListener jako myParam::
+    let array = e.currentTarget.myParam;
+
+    for (let i=0; i<array.length; i++){
+
+        if(array[i].toLowerCase() === searchInput.value.toLowerCase()) {
+            console.log("ok");
+            titleDisplay.textContent = array[i];
+            const newTitleDiv = document.createElement("div");
+            document.body.appendChild(newTitleDiv);
+            newTitleDiv.classList.add("title-display");
+            newTitleDiv.textContent = array[i];
+            searchInput.value = "";
+            titleDisplay.textContent = "";
+            return
+        } else if (array[i].toLowerCase() !== searchInput.value.toLowerCase() && searchInput.value !== "") {
+            titleDisplay.textContent = "Title not found";
+            
+        }
+    }
 }
 
-
+searchButton.myParam = favTitles;
 searchButton.addEventListener("click", searchTitles);
