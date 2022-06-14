f1cd887be782b68660e126bf41c7eac38b5ce080
MJanuszek <monika.januszek.mail@gmail.com>
2022-06-14 12:29:18 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// div to add titles:
+const titleDisplay = document.querySelector(".title-display");
+const searchInput = document.querySelector(".search-input");
+const searchButton = document.querySelector(".search-button");
+
+
+
+
+const searchTitles = () => {
+    console.log("ok")
+}
+
+
+searchButton.addEventListener("click", searchTitles);
